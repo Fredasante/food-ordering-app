33125d05660b5d23cf5c9718f1ff5ed7c2eb697f
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-05-31 19:04:15 +0000

    Configured User Authentication with database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import userRoute from "./routes/MyUserRoute";
+import myUserRoute from "./routes/MyUserRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/my/user", userRoute);
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
---
+++ frontend/src/api/MyUserApi.tsx
@@ -0,0 +1,31 @@
+import { useMutation } from "react-query";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+type createUserRequest = {
+  auth0Id: string;
+  email: string;
+};
+
+export const useCreateMyUser = () => {
+  const createMyUserRequest = async (user: createUserRequest) => {
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to create user");
+    }
+  };
+  const {
+    mutateAsync: createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useMutation(createMyUserRequest);
+
+  return { createUser, isLoading, isError, isSuccess };
+};
