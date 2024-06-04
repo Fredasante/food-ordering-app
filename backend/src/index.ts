234e32567ab36b5b3c0ab28caffb8f8498da3bc0
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-06-04 12:23:01 +0000

    Backend Deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
+
 app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
