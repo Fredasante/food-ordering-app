5d45da4c48708d79c509811e43402277f41453fa
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-06-04 00:47:21 +0000

    Added get current user request

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,8 +1,35 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useGetMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+  const getMyUserRequest = async () => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Failed to get user");
+    }
+    return response.json();
+  };
+  const {
+    data: currentUser,
+    isLoading,
+    error,
+  } = useQuery("fetchCurrentUser", getMyUserRequest);
+
+  if (error) {
+    toast.error(error.toString());
+  }
+
+  return { currentUser, isLoading };
+};
 
 type createUserRequest = {
   auth0Id: string;
