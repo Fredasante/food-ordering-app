29559839cd27badcc9b6951557e78b353b1da134
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-06-14 17:39:56 +0000

    Added Update Restaurant endpoint

--- backend/src/routes/MyRestaurantRoute.ts
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -22,4 +22,13 @@ router.post(
   MyRestaurantController.createMyRestaurant
 );
 
+router.put(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant
+);
+
 export default router;
