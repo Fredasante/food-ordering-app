3f3c468f17cf573ecf5ab140347699f77053e334
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-06-16 08:42:15 +0000

    Search Request Hook Added

---
+++ frontend/src/api/RestaurantApi.tsx
@@ -0,0 +1,22 @@
+import { useQuery } from "react-query";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useSearchRestaurants = (city?: string) => {
+  const createSearchRequest = async () => {
+    const response = await fetch(
+      `${API_BASE_URL}/api/restaurant/search/${city}`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch restaurants");
+    }
+    return response.json();
+  };
+  const { data: results, isLoading } = useQuery(
+    ["searchRestaurants"],
+    createSearchRequest,
+    { enabled: !!city }
+  );
+
+  return { results, isLoading };
+};
--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,9 +1,12 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
   const { city } = useParams();
+  const { results } = useSearchRestaurants(city);
+
   return (
-    <div className="container mx-auto px-0">
+    <div className="container mx-auto">
       <h1>Search Page</h1>
       <p>City: {city}</p>
     </div>
