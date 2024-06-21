7d470c2be0449bc0dc2b8416a79898959fc26b8b
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-06-21 15:23:52 +0000

    Added Restaurant Order Info for managers

---
+++ frontend/src/components/OrderItemCard.tsx
@@ -0,0 +1,95 @@
+import { Order } from "@/types";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Badge } from "./ui/badge";
+import { Label } from "./ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ORDER_STATUS } from "@/config/order-status-config";
+
+type Props = {
+  order: Order;
+};
+
+const OrderItemCard = ({ order }: Props) => {
+  const getTime = () => {
+    const orderDateTime = new Date(order.createdAt);
+
+    const hours = orderDateTime.getHours();
+    const minutes = orderDateTime.getMinutes();
+
+    const paddedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    return `${hours}:${paddedMinutes}`;
+  };
+
+  console.log(order);
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="grid md:grid-cols-4 gap-4 justify-between mb-3">
+          <div>
+            Customer Name:
+            <span className="ml-2 font-normal text-sm">
+              {order.deliveryDetails.name}
+            </span>
+          </div>
+          <div>
+            Delivery Address:
+            <span className="ml-2 font-normal text-sm">
+              {order.deliveryDetails.addressLine1}, {order.deliveryDetails.city}
+            </span>
+          </div>
+          <div>
+            Time:
+            <span className="ml-2 font-normal text-sm">{getTime()}</span>
+          </div>
+          <div>
+            Total Cost:
+            <span className="ml-2 font-normal text-sm">
+              {(order.totalAmount / 100).toFixed(2)}
+            </span>
+          </div>
+        </CardTitle>
+        <hr className="border-gray-300" />
+      </CardHeader>
+
+      <CardContent>
+        {order.cartItems.map((item) => (
+          <div key={item.menuItemId} className="flex justify-between">
+            <div>
+              <Badge className="mr-3 bg-teal-600 hover:bg-teal-600">
+                x{item.quantity}
+              </Badge>
+
+              <span className="font-semibold">{item.name}</span>
+            </div>
+          </div>
+        ))}
+
+        <div className="flex flex-col space-y-1.5 mt-5 md:mt-10">
+          <Label htmlFor="status" className="mb-1">
+            What is the status of this order?
+          </Label>
+          <Select>
+            <SelectTrigger id="status">
+              <SelectValue placeholder="Select order status" />
+            </SelectTrigger>
+            <SelectContent position="popper">
+              {ORDER_STATUS.map((status) => (
+                <SelectItem key={status.label} value={status.value}>
+                  {status.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default OrderItemCard;
--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,9 +1,12 @@
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
+  useGetMyRestaurantOrders,
   useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import OrderItemCard from "@/components/OrderItemCard";
 
 const ManageRestaurantPage = () => {
   const { createRestaurant, isLoading: isCreateLoading } =
@@ -11,16 +14,37 @@ const ManageRestaurantPage = () => {
   const { restaurant } = useGetMyRestaurant();
   const { updateRestaurant, isLoading: isUpdateLoading } =
     useUpdateMyRestaurant();
+  const { orders, isLoading: isOrdersLoading } = useGetMyRestaurantOrders();
+
+  console.log(orders);
 
   const isEditing = !!restaurant;
   return (
-    <div>
-      <ManageRestaurantForm
-        restaurant={restaurant}
-        onSave={isEditing ? updateRestaurant : createRestaurant}
-        isLoading={isCreateLoading || isUpdateLoading}
-      />
-    </div>
+    <Tabs
+      defaultValue="orders"
+      className="container mx-auto min-h-[85vh] md:mt-10 mt-5"
+    >
+      <TabsList>
+        <TabsTrigger value="orders">Orders</TabsTrigger>
+        <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+      </TabsList>
+      <TabsContent
+        value="orders"
+        className="space-y-5 bg-gray-50 p-5 md:px-10 rounded-lg"
+      >
+        <h2 className="text-md font-bold">{orders?.length} Active Orders</h2>
+        {orders?.map((order) => (
+          <OrderItemCard key={order._id} order={order} />
+        ))}
+      </TabsContent>
+      <TabsContent value="manage-restaurant">
+        <ManageRestaurantForm
+          restaurant={restaurant}
+          onSave={isEditing ? updateRestaurant : createRestaurant}
+          isLoading={isCreateLoading || isUpdateLoading}
+        />
+      </TabsContent>
+    </Tabs>
   );
 };
 
