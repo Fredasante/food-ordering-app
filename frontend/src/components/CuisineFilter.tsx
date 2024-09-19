dc2b6d38dad00ffc3895fc7545e4e4dc91e24e50
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-09-19 12:42:04 +0000

    update: cuisines filter responsive styles

--- frontend/src/components/CuisineFilter.tsx
+++ frontend/src/components/CuisineFilter.tsx
@@ -59,7 +59,7 @@ const CuisineFilter = ({
                 />
                 <Label
                   htmlFor={`cuisine_${cuisine}`}
-                  className={`flex flex-1 items-center text-sm cursor-pointer rounded-full px-4 py-2 font-semibold ${
+                  className={`flex flex-1 items-center text-sm cursor-pointer rounded-full px-5 py-2 font-semibold ${
                     isSelected
                       ? "border border-green-600 text-green-600"
                       : "border border-slate-300 text-gray-600"
@@ -75,7 +75,7 @@ const CuisineFilter = ({
         <Button
           variant="link"
           onClick={onExpandedClick}
-          className="mt-4 flex-1"
+          className="mt-5 flex-1"
         >
           {isExpanded ? (
             <span className="flex items-center">
